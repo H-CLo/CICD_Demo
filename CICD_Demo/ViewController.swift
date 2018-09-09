3985d08ec4e0a158d02273812feb2f3bbe64429f
H-CLo <nba1895@gmail.com>
2018-09-09 23:40:28 +0800

    first commit

---
+++ CICD_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CICD_Demo
+//
+//  Created by Hung Chang Lo on 2018/9/9.
+//  Copyright © 2018年 Hung Chang Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
